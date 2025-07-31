48d5b23000cbed18b1c5f7a88de048b6977d2aa4
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-07-31 16:49:15 +0200

    implemented function show in controller

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # webapp-express
 
-- Aiutandoci con il file in allegato, creiamo un database con MySQL Workbench
-- Creiamo una nuova applicazione Express
-- Colleghiamo l’app al db e verifichiamo che tutto funzioni
-- Prepariamo una rotta index per ottenere la lista dei film
-- Prepariamo una rotta show per ottenere i dettagli di un singolo film e le sue recensioni
+-- Aiutandoci con il file in allegato, creiamo un database con MySQL Workbench
+-- Creiamo una nuova applicazione Express
+-- Colleghiamo l’app al db e verifichiamo che tutto funzioni
+-- Prepariamo una rotta index per ottenere la lista dei film
+-- Prepariamo una rotta show per ottenere i dettagli di un singolo film e le sue recensioni
 
 
 # Bonus
-- Inserire i dati di connessione al database come variabili d’ambiente
+-- Inserire i dati di connessione al database come variabili d’ambiente
 - Inserire un middleware per le rotte inesistenti
 - Inserire un middleware per la gestione errori
 - Inserire delle immagini nel progetto express e dunque nel db
-- Inserire le vostre API in controller
-- Inserire le vostre rotte in un router
+-- Inserire le vostre API in controller
+-- Inserire le vostre rotte in un router
--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -4,7 +4,7 @@ const connection = require('../database/connection');
 //definisco le rotte che poi andrò ad usare in router
 
 function index(req, res) {
-    const sql = 'SELECt * FROM movies';
+    const sql = 'SELECT * FROM movies';
     connection.execute(sql, (err, result) => {
         if (err) return res.status(500).json({
             error: true,
@@ -16,10 +16,34 @@ function index(req, res) {
 }
 
 function show(req, res) {
-
+    const { id } = req.params;
+    const sql = 'SELECT * FROM movies WHERE id = ?';
+    connection.execute(sql, [id], (err, result) => {
+        if (err) return res.status(500).json({
+            error: true,
+            message: err.message
+        })
+        if (result.length === 0) {
+            return res.status(404).json({
+                error: true,
+                message: 'Not Found'
+            })
+        }
+        const movie = result[0];
+        const reviewsSql = 'SELECT * FROM reviews WHERE movie_id = ?';
+        connection.execute(reviewsSql, [id], (err, result) => {
+            if (err) return res.status(500).json({
+                error: true,
+                message: err.message
+            })
+            const movieReviews = result;
+            movie.result = movieReviews;
+            res.json(movie);
+        })
+    })
 }
 
 module.exports = {
     index,
     show
-}
+};
